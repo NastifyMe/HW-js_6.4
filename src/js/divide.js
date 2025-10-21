8ff122156bd04d2c6b21614b0daea56819d42990
NastifyMe <suprunar2010@gmail.com>
2025-10-21 15:08:45 +0200

    Updates

--- src/js/divide.js
+++
@@ -1,8 +0,0 @@
-export default function divide(a, b, m) {
-    if (b !== 0){
-        return a / b
-    } else {
-        return 'На 0 ділити не можна'
-    }
-}
-
--- src/js/multiply.js
+++
@@ -1,4 +0,0 @@
-export default function multiply(a, b) {
-    return a * b
-}
-
